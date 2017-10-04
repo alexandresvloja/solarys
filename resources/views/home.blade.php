5b250634cdefe59b4ec109864b381a95ce6b978b
alexandre <alexandre@serveloja.com.br>
2017-10-04 16:52:35 -0300

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alexandre
- * Date: 04/10/2017
- * Time: 16:25
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Company-HTML Bootstrap theme</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" />	
+</head>
+<header>        
+@include('includes/navbar')  
+@include('includes/section_como_fazemos') 
+@include('includes/section_como_funciona') 
+</header>
+<body>
+</body>
+
+</html>
---
+++ resources/views/includes/section_como_fazemos.blade.php
@@ -0,0 +1,29 @@
+<br><br>
+<div class="feature">
+	<div class="container">
+		<div class="text-center">
+			<h3 style="font-size: 400%">O QUE FAZEMOS?</h3>
+			<div class="col-md-4">
+				<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms" >
+					<i class="fa fa-book"></i>	
+					<h2>Projeto personalizados</h2>
+					<h5>Com sua conta de luz e algumas informações, nossa equipe de engenharia faz o dimensionamento preliminar do seu projeto.</h5>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms" >
+					<i class="fa fa-laptop"></i>	
+					<h2>Consultoria</h2>
+					<h5>Oferecemos consultoria especializada para arquitetos e engenheiros civis que busquem por atualização na área de energia solar.</h5>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="hi-icon-wrap hi-icon-effect wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="900ms" >
+					<i class="fa fa-heart-o"></i>	
+					<h2>Instalação Completa</h2>
+					<h5>Fazemos a instalação com todos os procedimentos de segurança e seu sistema começa a funcionar e gerar a própria energia.</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
